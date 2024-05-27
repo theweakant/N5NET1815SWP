659c938c1fd6cba1278e5fa6b0582e969746cf0c
phonglam <lamphong1507@gmail.com>
2024-05-27 13:11:56 +0700

    Revert "Update LoginPage,RegisterPage,RememberPage and Update Breadcrumb, blogPage"
    This reverts commit 4da38a3c72b46d1a11da2c313ca752e22798a1c4.

---
+++ src/routes/index.jsx
@@ -0,0 +1,11 @@
+export const routes = {
+  home: "/",
+  login: "/login",
+  register: "/register",
+  forgot: "/forgotpassword",
+  faq: "/faq",
+  size: "/size",
+  bst: "/bo-suu-tap",
+  admin: "/admin",
+  blog: "/blog",
+};
