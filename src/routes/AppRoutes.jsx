de70270d53540cd806f0b0d05fc14844e8c92d7a
JenbeJB <anhntse182560@fpt.edu.vn>
2024-06-06 09:24:28 +0700

    ProductDetail

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -19,6 +19,7 @@ import WarrantyPolicyPage from "../pages/WarrantyPolicyPage/WarrantyPolicyPage";
 import AdminProduct from "../pages/AdminDashboard/AdminProduct/AdminProduct";
 import AdminDiamond from "../pages/AdminDashboard/AdminPage/AdminPageDiamond";
 import AdminManageOrder from "../pages/AdminDashboard/AdminManageOrder/AdmiManageOrder";
+import ProductDetailPage from "../pages/ProductPage/ProductDetailPage";
 
 export default function AppRoute() {
   return (
@@ -42,6 +43,7 @@ export default function AppRoute() {
       <Route path={routes.adminDiamond} element={<AdminDiamond />} />
       <Route path={routes.adminDiamond} element={<AdminDiamond />} />
       <Route path={routes.adminmanageorder} element={<AdminManageOrder />} />
+      <Route path={routes.product} element={<ProductDetailPage />} />
     </Routes>
   );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -17,4 +17,5 @@ export const routes = {
   adminProduct: "/admin-san-pham",
   adminDiamond: "/admin-kim-cuong",
   adminmanageorder: "/admin-quan-li-don-hang",
+  product: "/san-pham",
 };
